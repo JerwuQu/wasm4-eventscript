4a89be3a70d1932c377d403b81d170fd6ccb0a74
Marcus Ramse <marcus@ramse.se>
2022-08-07 18:07:14 +0200

    Refactor into new script system

--- src/eventscript.zig
+++ src/eventscript.zig
@@ -1,46 +1,105 @@
 const std = @import("std");
 
-fn MakeEventCall(comptime T: type, entry: anytype) type {
-    const func = entry[0];
-    const args = if (entry.len > 1) entry[1] else .{};
+fn MakeEventInit(comptime SystemType: type, comptime ScriptType: type, entry: anytype) type {
+    const funcName = entry[0];
     return struct {
-        pub fn call(scriptInst: *T) void {
-            var params = .{scriptInst} ++ args;
-            @call(.{}, func, params);
+        pub fn call(scriptInst: *ScriptType) void {
+            if (@hasField(SystemType.EventStateUnion, funcName)) {
+                scriptInst.eventState = @unionInit(SystemType.EventStateUnion, funcName, .{});
+            }
         }
     };
 }
 
-pub fn Script(eventList: anytype) type {
+fn MakeEventTick(comptime SystemType: type, comptime ScriptType: type, entry: anytype) type {
+    const funcName = entry[0];
+    const args = if (entry.len > 1) entry[1] else .{};
     return struct {
-        const ScriptType = @This();
-
-        const events = eventsBlk: {
-            var eventFns: [eventList.len]fn (*ScriptType) void = undefined;
-            for (eventList) |eventListEntry, i| {
-                eventFns[i] = MakeEventCall(ScriptType, eventListEntry).call;
+        pub fn call(scriptInst: *ScriptType) void {
+            if (@hasField(SystemType.EventStateUnion, funcName)) {
+                var params = .{scriptInst, &@field(scriptInst.eventState, funcName)} ++ args;
+                @call(.{}, @field(SystemType.Def, funcName), params);
+            } else {
+                var params = .{scriptInst} ++ args;
+                @call(.{}, @field(SystemType.Def, funcName), params);
             }
-            break :eventsBlk eventFns;
+        }
+    };
+}
+
+pub fn System(comptime systemDef: type) type {
+    return struct {
+        const SystemType = @This();
+        const Def = systemDef;
+
+        // Union of all possible event state types with the function as name
+        // TODO: handle systems without any event states
+        const EventStateUnion: type = blk: {
+            var fields: []const std.builtin.Type.UnionField = &.{};
+            for (@typeInfo(Def).Struct.decls) |fnDecl| {
+                const fnDeclField = @field(Def, fnDecl.name);
+                if (@typeInfo(@TypeOf(fnDeclField)) == .Fn) {
+                    const stateDeclName = fnDecl.name ++ "State";
+                    if (@hasDecl(Def, stateDeclName)) {
+                        const stateDecl = @field(Def, stateDeclName);
+                        fields = fields ++ [_]std.builtin.Type.UnionField{.{
+                            .name = fnDecl.name,
+                            .field_type = stateDecl,
+                            .alignment = @alignOf(stateDecl),
+                        }};
+                    }
+                }
+            }
+            break :blk @Type(std.builtin.Type{ .Union = .{
+                .layout = .Auto,
+                .tag_type = null,
+                .fields = fields,
+                .decls = &.{},
+            } });
         };
 
-        kept: bool = false,
-        eventI: usize = 0,
-        eventTick: usize = 0,
+        pub fn Script(eventList: anytype) type {
+            return struct {
+                const ScriptType = @This();
 
-        pub fn keep(self: *ScriptType) void {
-            self.kept = true;
-        }
-        pub fn tick(self: *ScriptType) void {
-            while (self.eventI < events.len) {
-                self.kept = false;
-                events[self.eventI](self);
-                self.eventTick += 1;
-                if (self.kept) {
-                    break;
+                const EventFns = struct {
+                    init: fn (*ScriptType) void,
+                    tick: fn (*ScriptType) void,
+                };
+
+                const events = eventsBlk: {
+                    var eventFns: [eventList.len]EventFns = undefined;
+                    for (eventList) |entry, i| {
+                        eventFns[i].init = MakeEventInit(SystemType, ScriptType, entry).call;
+                        eventFns[i].tick = MakeEventTick(SystemType, ScriptType, entry).call;
+                    }
+                    break :eventsBlk eventFns;
+                };
+
+                kept: bool = false,
+                eventI: usize = 0,
+                eventTick: usize = 0,
+                eventState: EventStateUnion = undefined,
+
+                pub fn keep(self: *ScriptType) void {
+                    self.kept = true;
                 }
-                self.eventI += 1;
-                self.eventTick = 0;
-            }
+                pub fn tick(self: *ScriptType) void {
+                    while (self.eventI < events.len) {
+                        self.kept = false;
+                        if (self.eventTick == 0) {
+                            events[self.eventI].init(self);
+                        }
+                        events[self.eventI].tick(self);
+                        self.eventTick += 1;
+                        if (self.kept) {
+                            break;
+                        }
+                        self.eventI += 1;
+                        self.eventTick = 0;
+                    }
+                }
+            };
         }
     };
 }
--- src/example.zig
+++ src/example.zig
@@ -35,62 +35,67 @@ fn dialogueTextWrap(txt: []const u8, x: i32, y: i32, w: usize, charsShown: usize
     return row * 8;
 }
 
-fn wait(s: anytype, duration: usize) void {
-    if (s.eventTick + 1 < duration) {
-        s.keep();
-    }
-}
-
-var gReqPresses: u8 = 0;
-fn requirePresses(s: anytype, amount: u8) void {
-    if (padCheck(w4.BUTTON_1)) {
-        gReqPresses += 1;
-    }
-    if (gReqPresses < amount) {
-        w4.text("Presses left: ", 10, 40);
-        var buf: [2]u8 = undefined;
-        buf[0] = '0' + amount - gReqPresses;
-        buf[1] = 0;
-        w4.text(buf[0..2], 130, 40);
-        s.keep();
-    }
-}
-fn showText(s: anytype, text: []const u8, duration: u8) void {
-    if (s.eventTick < duration) {
-        w4.text(text, 20, 20);
-        s.keep();
-    }
-}
-
-fn dialogue(s: anytype, text: []const u8) void {
-    if (!padCheck(w4.BUTTON_1)) {
-        w4.text("Dialogue", 20, 10);
-        const shownChars = s.eventTick / 2;
-        const allShown = shownChars >= text.len;
-        const h = dialogueTextWrap(text, 5, 22, 150, if (allShown) text.len else shownChars);
-        if (allShown and (s.eventTick / 30) % 2 == 0) {
-            w4.text("\x80", 20, 24 + h);
+const mySystem = eventscript.System(struct {
+    pub fn wait(s: anytype, duration: usize) void {
+        if (s.eventTick + 1 < duration) {
+            s.keep();
         }
+    }
+
+    pub const requirePressesState = struct {
+        presses: u8 = 0,
+    };
+    pub fn requirePresses(s: anytype, state: *requirePressesState, amount: u8) void {
+        if (padCheck(w4.BUTTON_1)) {
+            state.presses += 1;
+        }
+        if (state.presses < amount) {
+            w4.text("Presses left: ", 10, 40);
+            var buf: [2]u8 = undefined;
+            buf[0] = '0' + amount - state.presses;
+            buf[1] = 0;
+            w4.text(buf[0..2], 130, 40);
+            s.keep();
+        }
+    }
+
+    pub fn showText(s: anytype, text: []const u8, duration: u8) void {
+        if (s.eventTick < duration) {
+            w4.text(text, 20, 20);
+            s.keep();
+        }
+    }
+
+    pub fn dialogue(s: anytype, text: []const u8) void {
+        if (!padCheck(w4.BUTTON_1)) {
+            w4.text("Dialogue", 20, 10);
+            const shownChars = s.eventTick / 2;
+            const allShown = shownChars >= text.len;
+            const h = dialogueTextWrap(text, 5, 22, 150, if (allShown) text.len else shownChars);
+            if (allShown and (s.eventTick / 30) % 2 == 0) {
+                w4.text("\x80", 20, 24 + h);
+            }
+            s.keep();
+        }
+    }
+
+    pub fn restartScript(s: anytype) void {
+        s.eventI = 0;
+        s.eventTick = 0;
         s.keep();
     }
-}
+});
 
-fn restartScript(s: anytype) void {
-    s.eventI = 0;
-    s.eventTick = 0;
-    s.keep();
-}
-
-const MyScript = eventscript.Script(.{
-    .{dialogue, .{"Hi!"}},
-    .{dialogue, .{"Gonna need ya to press \x80 a few times, ok?"}},
-    .{requirePresses, .{5}},
-    .{showText, .{"Hold on.", 60}},
-    .{showText, .{"Hold on..", 60}},
-    .{showText, .{"Hold on...", 60}},
-    .{dialogue, .{"Good job!"}},
-    .{dialogue, .{"Time to restart?"}},
-    .{restartScript},
+const MyScript = mySystem.Script(.{
+    .{"dialogue", .{"Hi!"}},
+    .{"dialogue", .{"Gonna need ya to press \x80 a few times, ok?"}},
+    .{"requirePresses", .{5}},
+    .{"showText", .{"Hold on.", 60}},
+    .{"showText", .{"Hold on..", 60}},
+    .{"showText", .{"Hold on...", 60}},
+    .{"dialogue", .{"Good job!"}},
+    .{"dialogue", .{"Time to restart?"}},
+    .{"restartScript"},
 });
 
 var script = MyScript{};
