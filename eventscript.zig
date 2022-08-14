e1861744d3087f217b44194b5b5252d090160e5d
Marcus Ramse <marcus@ramse.se>
2022-08-14 21:05:38 +0200

    Add `currentScript`

--- eventscript.zig
+++ eventscript.zig
@@ -98,6 +98,12 @@ pub fn System(comptime systemDef: type) type {
             pub fn init(stack: []ScriptCtx) Runner {
                 return Runner{ .stack = stack };
             }
+            pub fn currentScript(self: *Runner) ?*const Script {
+                if (self.stackSize > 0) {
+                    return self.stack[self.stackSize - 1].script;
+                }
+                return null;
+            }
             pub fn startScript(self: *Runner, script: *const Script) void {
                 self.stackSize = 0;
                 self.callScript(script);
